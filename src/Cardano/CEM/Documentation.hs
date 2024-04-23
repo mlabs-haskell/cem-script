f87a21a9e08855834e746851e812e62e284c229b
Gregory Gerasev <uhbif19@gmail.com>
2024-04-23 10:34:34 +0700

    Fix linters in `Cardano.CEM.Documentation`

--- src/Cardano/CEM/Documentation.hs
+++ src/Cardano/CEM/Documentation.hs
@@ -1,13 +1,14 @@
-module Cardano.CEM.Documentation where
+module Cardano.CEM.Documentation (cemDotGraphString) where
 
 import Prelude
 
 import Data.Map qualified as Map
 import Data.Proxy
+import Data.Foldable (fold)
 
 import Cardano.CEM
-import Data.List (stripPrefix)
 
+dotStyling :: String
 dotStyling =
   "rankdir=LR;\n"
     <> "node [shape=\"dot\",fontsize=14,fixedsize=true,width=1.5];\n"
@@ -28,17 +29,16 @@ cemDotGraphString name proxy =
     showSpine = stripSpineSuffix . show
     stripSpineSuffix = reverse . drop 5 . reverse
     edges =
-      foldMap id $
-        [ ( maybe "\"Void In\"" showSpine from
+      fold $
+        [ maybe "\"Void In\"" showSpine from
               <> " -> "
-              <> (maybe "\"Void Out\"" showSpine to)
+              <> maybe "\"Void Out\"" showSpine to
               <> " [label=\""
               <> showSpine transition
               <> " (stage "
               <> show stage
               <> ")"
               <> "\"]; \n"
-          )
         | (transition, (stage, from, to)) <-
             Map.assocs $ transitionStage proxy
         ]
