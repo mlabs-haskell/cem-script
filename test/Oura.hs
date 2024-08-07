5b7df39442f38d5318d1447ad9c0c9971bd43346
Renegatto <edittorr@mail.ru>
2024-08-07 21:20:03 +0300

    Make tooling for receiving data from oura

--- test/Oura.hs
+++ test/Oura.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE BlockArguments #-}
 module Oura
   ( connectToDaemon
   , sendToOura
   , close
   , exampleTx
+
+  , listenOuraSink
+  , stopMonitoring
+  , waitForOutput
   ) where
 
 import Prelude
 import qualified Network.Socket as Socket
 import qualified Data.ByteString as BS
 import qualified Network.Socket.ByteString as Socket.BS
+import Control.Concurrent (Chan, writeList2Chan, readChan, threadDelay, newChan, ThreadId, forkIO, killThread, myThreadId)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as Text.Encoding
+import Control.Monad (forever, void, replicateM)
+import Data.Traversable (for)
+import Data.Foldable (for_)
 
 exampleTx :: IO BS.ByteString
 exampleTx = BS.readFile "./tx.json"
@@ -45,3 +56,46 @@ close MkOuraDaemonConnection {ownSocket} = do
   let
     timeoutMs = 2_000
   Socket.gracefulClose ownSocket timeoutMs
+
+-- * Consuming Oura output
+
+data OuraOutput = MkOuraOutput
+  { output :: Chan T.Text
+  , sinkPath :: FilePath
+  , monitor :: Maybe ThreadId
+  }
+
+newtype Interval = MkIntervalMs { unIntervalMs :: Int }
+
+listenOuraSink :: FilePath -> Maybe Interval -> IO OuraOutput
+listenOuraSink sinkPath monitoringInterval = do
+  output <- newChan
+  let
+    mkOutput monitor = MkOuraOutput
+      { output
+      , sinkPath
+      , monitor
+      }
+  mkOutput <$> for monitoringInterval \interval -> forkIO do
+    monitor <- myThreadId
+    monitorOutputs interval $ mkOutput $ Just monitor
+
+stopMonitoring :: OuraOutput -> IO ()
+stopMonitoring MkOuraOutput {monitor} = for_ monitor killThread
+
+waitForOutput :: OuraOutput -> IO T.Text
+waitForOutput out@MkOuraOutput {output} = do
+  collectOutput out
+  readChan output
+
+monitorOutputs :: Interval -> OuraOutput -> IO a
+monitorOutputs MkIntervalMs {unIntervalMs} out = forever do
+  collectOutput out
+  let microSecondsInMillisecond = 1_000
+  threadDelay $ microSecondsInMillisecond * unIntervalMs
+
+collectOutput :: OuraOutput -> IO ()
+collectOutput MkOuraOutput {output, sinkPath} = do
+  contents <- BS.readFile sinkPath
+  BS.writeFile sinkPath ""
+  writeList2Chan output $ reverse $ T.lines $ Text.Encoding.decodeUtf8 contents
