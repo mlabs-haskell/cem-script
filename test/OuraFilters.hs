2bff8c1786f3ba5c72cf9da23e933bf9147e26bc
Renegatto <edittorr@mail.ru>
2024-08-21 20:29:24 +0300

    Test that Oura is functioning

--- test/OuraFilters.hs
+++ test/OuraFilters.hs
@@ -1,11 +1,48 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module OuraFilters (ouraFiltersSpec) where
 
 import Prelude
+import Oura (Oura (send, receive, shutDown))
+import Oura qualified
+import Test.Hspec (Spec, it, shouldBe)
+import Control.Monad ((>=>))
+import qualified Data.Text as T
+import qualified Data.Text.IO as T.IO
+import Utils qualified
+import qualified Data.Aeson.Types as Aeson
+import qualified Data.Aeson as Aeson
+import Data.Aeson ((.:))
 
-import Test.Hspec (describe, it, shouldBe, Spec)
+exampleTx :: IO T.Text
+exampleTx = T.IO.readFile "./tx.json"
+
+exampleMatchingTx :: IO T.Text
+exampleMatchingTx = T.IO.readFile "./matchingTx.json"
 
 ouraFiltersSpec :: Spec
-ouraFiltersSpec = do
-  it "Can execute oura" do
-    fail @IO @() "not implemented"
+ouraFiltersSpec = Utils.killProcessesOnError do --hspec do
+  it "Can execute oura" \spotGarbage -> do
+    Oura.withOura (Oura.MkWorkDir "./tmp") spotGarbage \oura -> do
+      tx <- exampleTx
+      matchingTx <- exampleMatchingTx
+      oura.send tx
+      -- _ <- oura.receive
+      oura.send matchingTx
+      Right outTxHash
+        <- extractOutputTxHash <$> oura.receive
+      Right inputTxHash
+        <- pure $ extractInputTxHash matchingTx
+      outTxHash `shouldBe` inputTxHash
+      oura.shutDown
+
+extractInputTxHash :: T.Text -> Either String T.Text
+extractInputTxHash = Aeson.eitherDecodeStrictText >=> Aeson.parseEither \json -> do
+  parsedTx <- json .: "parsed_tx"
+  parsedTx .: "hash"
+
+extractOutputTxHash :: T.Text -> Either String T.Text
+extractOutputTxHash = Aeson.eitherDecodeStrictText >=> Aeson.parseEither \json -> do
+  parsedTx <- json .: "record"
+  parsedTx .: "hash"
