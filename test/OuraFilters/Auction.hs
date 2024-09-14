7a3dd8dd7b866fff1cfbd57e5a7921efc5c7a94f
Renegatto <edittorr@mail.ru>
2024-09-14 20:36:04 +0300

    WIP: Make simple indexer test

--- test/OuraFilters/Auction.hs
+++ test/OuraFilters/Auction.hs
@@ -6,7 +6,7 @@ module OuraFilters.Auction (spec) where
 import Cardano.CEM (CEMScriptDatum)
 import Cardano.CEM.Examples.Auction qualified as Auction
 import Control.Arrow ((>>>))
-import Control.Lens ((%~), (.~), (^.))
+import Control.Lens ((%~), (.~), (?~), (^.))
 import Data.Aeson qualified as Aeson
 import Data.Function ((&))
 import Data.Functor (void, (<&>))
@@ -24,8 +24,42 @@ import Prelude
 
 spec :: SpecM (SpotGarbage IO ProcessHandle) ()
 spec =
+  --  @(SpotGarbage IO ProcessHandle)
   describe "Auction example" do
-    focus $ it "Recognizes 'Create' transition" \spotGarbage -> do
+    focus $ it "Catches any Auction validator transition" \spotGarbage -> do
+      Oura.withOura @() (Oura.MkWorkDir "./tmp") spotGarbage \oura -> do
+        let
+          -- auction = undefined
+          auctionPaymentCredential = undefined
+          -- we want oura to monitor just payment credential, ignoring stake credentials
+          arbitraryStakeCredential = PlutusLedgerApi.V1.StakingPtr 5 3 2
+          defaultTx = Mock.arbitraryTx
+          rightTxHash =
+            Mock.MkBlake2b255Hex
+              "2266778888888888888888888888888888888888888888888888444444444444"
+          inputFromValidator =
+            emptyInputFixture auctionPaymentCredential (Just arbitraryStakeCredential)
+
+          tx =
+            Mock.txToBS
+              . Mock.mkTxEvent
+              . (Mock.inputs %~ (inputFromValidator :))
+              . (Mock.hash .~ rightTxHash)
+              $ defaultTx
+          unmatchingTx =
+            Mock.txToBS
+              . Mock.mkTxEvent
+              $ Mock.arbitraryTx
+
+        withTimeout 3.0 do
+          oura.send unmatchingTx
+          oura.send tx
+          Right txEvent <-
+            Aeson.eitherDecodeStrict @Mock.TxEvent
+              <$> oura.receive
+          (txEvent ^. Mock.parsed_tx . Mock.hash) `shouldBe` rightTxHash
+          oura.shutDown
+    it "Recognizes 'Create' transition" \spotGarbage -> do
       Oura.withOura (Oura.MkWorkDir "./tmp") spotGarbage \oura -> do
         let
           params =
@@ -51,7 +85,6 @@ spec =
         withTimeout 3.0 do
           oura.send unmatchingTx
           oura.send tx
-          -- 2 sec
           Right txEvent <-
             Aeson.eitherDecodeStrict @Mock.TxEvent
               <$> oura.receive
@@ -73,30 +106,18 @@ createTxMock params =
     & Mock.outputs %~ (:) output
   where
     input =
-      Mock.MkTxInput
-        { Mock._as_output =
-            Mock.MkTxOutput
-              { Mock._address =
-                  Mock.plutusAddressToOuraAddress $
-                    PlutusLedgerApi.V1.Address
-                      (PlutusLedgerApi.V1.PubKeyCredential params.seller)
-                      Nothing
-              , Mock._datum = Nothing -- any datum
-              , Mock._coin = 2
-              , Mock._script = Nothing
-              , Mock._assets = valueToMultiAsset params.lot
-              }
-        , Mock._tx_hash = Mock.MkBlake2b255Hex "af6366838cfac9cc56856ffe1d595ad1dd32c9bafb1ca064a08b5c687293110f"
-        , Mock._output_index = 0
-        , Mock._redeemer =
-            Just $
-              Mock.MkRedeemer
-                { _purpose = Mock.PURPOSE_SPEND
-                , datum =
-                    Mock.encodePlutusData $
-                      PlutusLedgerApi.V1.toData Auction.Create
-                }
-        }
+      emptyInputFixture
+        (PlutusLedgerApi.V1.PubKeyCredential params.seller)
+        Nothing
+        & Mock.as_output . Mock.assets .~ valueToMultiAsset params.lot
+        & Mock.redeemer
+          ?~ Mock.MkRedeemer
+            { _purpose = Mock.PURPOSE_SPEND
+            , datum =
+                Mock.encodePlutusData $
+                  PlutusLedgerApi.V1.toData Auction.Create
+            }
+
     output =
       Mock.MkTxOutput
         { Mock._address =
@@ -112,21 +133,43 @@ createTxMock params =
         , Mock._script = Nothing
         , Mock._assets = valueToMultiAsset params.lot
         }
-    valueToMultiAsset :: PlutusLedgerApi.V1.Value -> [Mock.Multiasset]
-    valueToMultiAsset =
-      PlutusLedgerApi.V1.getValue >>> AssocMap.toList >>> fmap \(cs, tokens) ->
-        Mock.MkMultiasset
-          { Mock._policy_id = Mock.serialiseCurrencySymbol cs
-          , Mock.assets =
-              AssocMap.toList tokens <&> \(tn, amt) ->
-                Mock.MkAsset
-                  { Mock._name =
-                      Mock.serialiseAsHex $
-                        PlutusLedgerApi.V1.unTokenName tn
-                  , Mock._output_coin = amt -- positive
-                  , Mock._mint_coin = 1
-                  }
-          }
 
     outputState :: CEMScriptDatum Auction.SimpleAuction
     outputState = (Auction.NoControl, params, Auction.NotStarted)
+
+valueToMultiAsset :: PlutusLedgerApi.V1.Value -> [Mock.Multiasset]
+valueToMultiAsset =
+  PlutusLedgerApi.V1.getValue >>> AssocMap.toList >>> fmap \(cs, tokens) ->
+    Mock.MkMultiasset
+      { Mock._policy_id = Mock.serialiseCurrencySymbol cs
+      , Mock.assets =
+          AssocMap.toList tokens <&> \(tn, amt) ->
+            Mock.MkAsset
+              { Mock._name =
+                  Mock.serialiseAsHex $
+                    PlutusLedgerApi.V1.unTokenName tn
+              , Mock._output_coin = amt -- positive
+              , Mock._mint_coin = 1
+              }
+      }
+
+emptyInputFixture ::
+  PlutusLedgerApi.V1.Credential ->
+  Maybe PlutusLedgerApi.V1.StakingCredential ->
+  Mock.TxInput
+emptyInputFixture paymentCred mstakeCred =
+  Mock.MkTxInput
+    { Mock._as_output =
+        Mock.MkTxOutput
+          { Mock._address =
+              Mock.plutusAddressToOuraAddress $
+                PlutusLedgerApi.V1.Address paymentCred mstakeCred
+          , Mock._datum = Nothing
+          , Mock._coin = 2
+          , Mock._script = Nothing
+          , Mock._assets = mempty
+          }
+    , Mock._tx_hash = Mock.MkBlake2b255Hex "af6366838cfac9cc56856ffe1d595ad1dd32c9bafb1ca064a08b5c687293110f"
+    , Mock._output_index = 0
+    , Mock._redeemer = Nothing
+    }
