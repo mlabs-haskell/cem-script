bf3a4d6abeef129c316e785e44e5b5a36603fe94
Renegatto <edittorr@mail.ru>
2024-09-14 22:26:21 +0300

    Make lenses for Toml to edit oura config

--- test/Oura/Config.hs
+++ test/Oura/Config.hs
@@ -1,11 +1,33 @@
+{-# LANGUAGE BlockArguments #-}
+
 module Oura.Config (
   daemonConfig,
   SourcePath (MkSourcePath, unSourcePath),
   SinkPath (MkSinkPath, unSinkPath),
+  filtersL,
+  tableL,
+  atKey,
+  _Table,
+  _Integer,
+  _Bool,
+  _Text,
 ) where
 
 import Prelude
 
+import Control.Lens (
+  At (at),
+  Each (each),
+  Lens',
+  Prism',
+  Traversal',
+  iso,
+  mapping,
+  partsOf,
+  prism',
+  _Just,
+ )
+import Data.Map (Map)
 import Data.String (IsString)
 import Data.Text qualified as T
 import Toml qualified
@@ -72,3 +94,44 @@ source (MkSourcePath socketPath) =
     [ "socket_path" .= Toml.Text socketPath
     , "type" .= Toml.Text "TxOverSocket"
     ]
+
+filtersL :: Traversal' Toml.Value [Toml.Table]
+filtersL =
+  _Table
+    . atKey "filters"
+    . _Just
+    . _List
+    . partsOf (each . _Table)
+
+atKey :: T.Text -> Traversal' Toml.Table (Maybe Toml.Value)
+atKey key = tableL . at key
+
+tableL :: Lens' Toml.Table (Map T.Text Toml.Value)
+tableL =
+  iso (\(Toml.MkTable t) -> t) Toml.MkTable
+    . mapping (iso snd ((),))
+
+_Table :: Prism' Toml.Value Toml.Table
+_Table = prism' Toml.Table \case
+  Toml.Table table -> Just table
+  _ -> Nothing
+
+_Text :: Prism' Toml.Value T.Text
+_Text = prism' Toml.Text \case
+  Toml.Text t -> Just t
+  _ -> Nothing
+
+_List :: Prism' Toml.Value [Toml.Value]
+_List = prism' Toml.List \case
+  Toml.List xs -> Just xs
+  _ -> Nothing
+
+_Bool :: Prism' Toml.Value Bool
+_Bool = prism' Toml.Bool \case
+  Toml.Bool b -> Just b
+  _ -> Nothing
+
+_Integer :: Prism' Toml.Value Integer
+_Integer = prism' Toml.Integer \case
+  Toml.Integer n -> Just n
+  _ -> Nothing
